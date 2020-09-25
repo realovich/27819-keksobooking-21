4395ff260b5230f2343e57197d4ae065278a3cd5
Tony <anton-rus@mail.ru>
2020-09-25 11:58:57 +0300

    Рефакторинг, часть 1

--- js/main.js
+++ js/main.js
@@ -1,23 +1,21 @@
 'use strict';
 
+const OFFER_TYPES = [`palace`, `flat`, `house`, `bungalow`];
+const OFFER_FEATURES = [`wifi`, `dishwasher`, `parking`, `washer`, `elevator`, `conditioner`];
+
 const mapElement = document.querySelector(`.map`);
 const mapWidth = mapElement.offsetWidth;
 
 const pinsListElement = mapElement.querySelector(`.map__pins`);
 const pinTemplate = document.querySelector(`#pin`).content.querySelector(`.map__pin`);
 
-const offerTypes = [`palace`, `flat`, `house`, `bungalow`];
-const offerFeatures = [`wifi`, `dishwasher`, `parking`, `washer`, `elevator`, `conditioner`];
-
-const getRandomInteger = (min, max) => {
-  return Math.floor(Math.random() * (max - min)) + min;
-};
+const getRandomInteger = (min, max) => Math.floor(Math.random() * (max - min)) + min;
 
 const renderAdArray = (length) => {
-  const array = new Array(length);
+  const array = [];
 
   for (let i = 0; i < length; i++) {
-    array[i] = {
+    array.push({
       'author': {
         'avatar': `0${i + 1}`
       },
@@ -25,12 +23,12 @@ const renderAdArray = (length) => {
         'title': `Заголовок`,
         'address': `600, 350`,
         'price': getRandomInteger(1000, 100000),
-        'type': offerTypes[getRandomInteger(0, offerTypes.length)],
+        'type': OFFER_TYPES[getRandomInteger(0, OFFER_TYPES.length)],
         'rooms': getRandomInteger(1, 4),
         'guests': getRandomInteger(0, 3),
         'checkin': `12:00`,
         'checkout': `13:00`,
-        'features': offerFeatures,
+        'features': OFFER_FEATURES,
         'description': `Описание объявления`,
         'photos': [`http://o0.github.io/assets/images/tokyo/hotel1.jpg`, `http://o0.github.io/assets/images/tokyo/hotel2.jpg`, `http://o0.github.io/assets/images/tokyo/hotel3.jpg`],
       },
@@ -38,7 +36,7 @@ const renderAdArray = (length) => {
         'x': getRandomInteger(1, mapWidth),
         'y': getRandomInteger(130, 630)
       }
-    };
+    });
   }
 
   return array;
@@ -66,7 +64,7 @@ const renderPinsList = () => {
     fragment.appendChild(renderPin(ads[i]));
   }
 
-  return pinsListElement.appendChild(fragment);
+  pinsListElement.appendChild(fragment);
 };
 
 mapElement.classList.remove(`map--faded`);
