77d5c43e8391f9d20e399d28bb70bb18f948d5fb
Tony <anton-rus@mail.ru>
2020-11-03 16:03:52 +0300

    Личный проект: перламутровые пуговицы (часть 2)

--- js/filter.js
+++ js/filter.js
@@ -1,23 +1,70 @@
 'use strict';
 
 (() => {
-  const FIELD_HOUSING_TYPE_ID = `#housing-type`;
-
   const filterForm = document.querySelector(`.map__filters`);
 
-  filterForm.addEventListener(window.util.Event.CHANGE, (evt) => {
-    const {target} = evt;
-    const {value} = target;
+  const housingType = filterForm.querySelector(`#housing-type`);
+  const housingPrice = filterForm.querySelector(`#housing-price`);
+  const housingRooms = filterForm.querySelector(`#housing-rooms`);
+  const housingGuests = filterForm.querySelector(`#housing-guests`);
+  const housingFeatures = filterForm.querySelector(`#housing-features`);
 
+  const onFilterFormChange = () => {
     window.card.closeAdCard();
 
-    if (target && target.matches(FIELD_HOUSING_TYPE_ID)) {
-      const typeFiltredAds = window.page.getSavedAds().filter((element) => value === `any` || element.offer.type === value);
+    const housingFeaturesElements = Array.from(housingFeatures.querySelectorAll(`.map__checkbox`));
 
-      window.page.saveFiltredAds(typeFiltredAds);
-      window.map.renderPinsList(typeFiltredAds);
-    }
-  });
+    const enabledHousingFeatures = housingFeaturesElements.filter((featuresValue) => {
+      return featuresValue.checked;
+    }).map((featuresValue) => {
+      return featuresValue.value;
+    });
+
+    const compareArray = (firstArray, secondArray) => {
+      for (let i = 0; i < firstArray.length; i++) {
+        if (!secondArray.includes(firstArray[i])) {
+          return false;
+        }
+      }
+
+      return true;
+    };
+
+    const priceTypeToValue = {
+      low: 10000,
+      high: 50000
+    };
+
+    const checkHousingPrice = (selectedOption, dataOption) => {
+      switch (selectedOption) {
+        case `low`:
+          return dataOption < priceTypeToValue.low;
+        case `middle`:
+          return dataOption >= priceTypeToValue.low && dataOption < priceTypeToValue.high;
+        case `high`:
+          return dataOption >= priceTypeToValue.high;
+        default:
+          return false;
+      }
+    };
+
+    const filteredAds = window.page.getSavedAds().filter((ad) => {
+      return housingType.value === `any` || ad.offer.type === housingType.value;
+    }).filter((ad) => {
+      return housingPrice.value === `any` || checkHousingPrice(housingPrice.value, ad.offer.price);
+    }).filter((ad) => {
+      return housingRooms.value === `any` || ad.offer.rooms === parseInt(housingRooms.value, 10);
+    }).filter((ad) => {
+      return housingGuests.value === `any` || ad.offer.guests === parseInt(housingGuests.value, 10);
+    }).filter((ad) => {
+      return enabledHousingFeatures.length === 0 || compareArray(enabledHousingFeatures, ad.offer.features);
+    });
+
+    window.page.saveFiltredAds(filteredAds);
+    window.map.renderPinsList(filteredAds);
+  };
+
+  filterForm.addEventListener(window.util.Event.CHANGE, window.debounce(onFilterFormChange));
 
   const resetForm = () => {
     filterForm.reset();
